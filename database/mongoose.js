1bfde4ecfb55ef50bac05bec400704491162d660
seth YALITA <cyalita@gmail.com>
2021-09-11 16:53:46 +1100

    added express and mongoose configuration

---
+++ database/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
+        .then(()=>{console.log("Db Connected Suceesfully!"
+
+        )})
+        .catch((error)=>{
+            console.log(error)
+        });
+
+ module.exports = mongoose;           
+
